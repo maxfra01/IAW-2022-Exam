3d8a57ad887eec863386142494ec927569919d8a
Massimo Francios <maxfra01@gmail.com>
2022-12-29 23:49:55 +0100

    add signup

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, request, session, redirect, flash, url_for
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, logout_user 
 from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 import dao
-import model
+from models.User import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mistero'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
-Session(app)
 
 login_manager=LoginManager()
 login_manager.init_app(app)
@@ -25,38 +24,66 @@ def load_user(user_id):
 
 @app.route("/")
 def home():
-   #categorie=dao.get_all_categories()  
-   #serie=dao.get_all_shows()
-   return render_template('home.html')
+   categorie=dao.get_all_categories()  
+   serie=dao.get_all_shows()
+   return render_template('home.html', categorie=categorie, serie=serie)
 
-@app.route("/login")
-def login():
-   return render_template('login.html')
+
+@app.route("/signup", methods=["POST"])
+def signup():
+   if request.method=="POST":
+      name = request.form.get('name')
+      email = request.form.get('email')
+      tipo=request.form.get('type')
+      password = request.form.get("password")
+      print(name,email, tipo, password)
+
+      user_in_db = dao.get_user_by_email(email,tipo)
+
+      if user_in_db:
+         flash('C\'è già un utente registrato con questa email', 'danger')
+         return redirect(url_for('home'))
+      else:
+         new_user = {
+            "name": name,
+            "email": email,
+            "tipo": tipo,
+            "password": generate_password_hash(password,method="pbkdf2:sha256")
+         }
+
+         success = dao.add_user(new_user)
+
+         if success:
+            flash('Utente creato correttamente', 'success')
+            return redirect(url_for('home'))
+         else:
+            flash('Errore nella creazione del utente: riprova!', 'danger')
+
+   return redirect(url_for('home'))
+
 
 
 @app.route("/login", methods=["POST"])
-def login_post():
+def login():
    if request.method=="POST":
       email=request.form.get("email")
       password=request.form.get("password")
-      tipo=request.form.get("tipo")
-      user_in_db=dao.get_user_by_email(email)
+      tipo=request.form.get("type")
+      
+      print(email, password, tipo)
+      
+      user_in_db=dao.get_user_by_email(email,tipo)
       if not user_in_db or not check_password_hash(user_in_db["password"], password):
-         flash("Credenziali non valide", 'danger')
-         return redirect(url_for('login'))
+         flash("Credenziali non valide", 'error')
+         return redirect(url_for('home'))
       else:
-         login_user()
+         new_user=User()
          
-   return
+         login_user(new_user)
+      
+   return redirect(url_for('home'))
             
-@app.route("/signup")
-def signup():
-   return render_template('signup.html')
 
-@app.route("/signup", methods=["POST"])
-def signup_post():
-   if request.method=="POST":
-      new_user=request.form.to_dict()
       
    
    
--- dao.py
+++ dao.py
@@ -26,6 +26,40 @@ def get_all_shows():
    conn.close()
    return shows
 
+def get_user_by_email(email,type):
+   conn=sqlite3.connect('db/podcast.db')
+   conn.row_factory = sqlite3.Row
+   cursor=conn.cursor()
+   if type=='creatore':
+      sql='SELECT * FROM creatore WHERE email=?'
+   else:
+      sql='SELECT * FROM ascoltatore WHERE email=?'
+   cursor.execute(sql, (email, ))
+   user=cursor.fetchone()
    
+   cursor.close()
+   conn.close()
+   return user
 
+def add_user(new_user):
+   conn=sqlite3.connect('db/podcast.db')
+   conn.row_factory = sqlite3.Row
+   cursor=conn.cursor()
+   success=False
    
+   if new_user["tipo"]=='creatore':
+      sql='INSERT INTO creatore(email, password, name) VALUES (?,?,?)'
+   else:
+      sql='INSERT INTO ascoltatore(email, password, name) VALUES (?,?,?)'
+   try:
+      cursor.execute(sql, (new_user['email'], new_user['password'], new_user['name']))
+      conn.commit()
+      success = True
+   except Exception as e:
+      print('ERROR', str(e))
+      conn.rollback()
+
+   cursor.close()
+   conn.close()
+   
+   return success
